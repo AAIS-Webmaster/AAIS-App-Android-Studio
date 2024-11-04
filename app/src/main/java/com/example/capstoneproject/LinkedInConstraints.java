322125a1a334e56fde85b9d5a15923e3ec1a7339
dhruvgupta2000 <guptasdhuruv4@gmail.com>
2024-11-04 17:45:37 +1100

    Final ACIS Android Application

--- app/src/main/java/com/example/capstoneproject/LinkedInConstraints.java
+++ app/src/main/java/com/example/capstoneproject/LinkedInConstraints.java
@@ -3,14 +3,14 @@ package com.example.capstoneproject;
 public class LinkedInConstraints {
 
     // LinkedIn OAuth 2.0 Client ID (Application-specific)
-    public static final String CLIENT_ID = "86p07gmy2sewyy";
+    public static final String CLIENT_ID = "Given in the LinkedIn App Auth tab";
 
     // LinkedIn OAuth 2.0 Client Secret (Application-specific)
-    public static final String CLIENT_SECRET = "PUZV8VOdJkWREdHu";
+    public static final String CLIENT_SECRET = "Given in the LinkedIn App Auth tab";
 
     // Redirect URI - Where LinkedIn will redirect the user after authorization
     // This should match the redirect URI registered in your LinkedIn app settings
-    public static final String REDIRECT_URI = "https://capstone-project-432506-default-rtdb.firebaseio.com/linkedin/callback";
+    public static final String REDIRECT_URI = "Specified when creating the LinkedIn App";
 
     // OAuth 2.0 Scopes - Defines the level of access requested from the user
     // Includes permissions to access profile info, social posts, and email
@@ -22,4 +22,3 @@ public class LinkedInConstraints {
     // Token URL - Used to exchange the authorization code for an access token
     public static final String TOKEN_URL = "https://www.linkedin.com/oauth/v2/accessToken";
 }
-
